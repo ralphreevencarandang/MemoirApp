523041ae3275c10ac79711528fb6e1f4801f13e2
Ralph Carandang <rvncrndng@gmail.com>
2025-06-22 18:52:03 +0800

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import noteRoutes from './routes/noteRoutes.js';
 import { connectDb } from './config/db.js';
 import rateLimiter from './middleware/rateLimiter.js';
 import dotenv from 'dotenv'
+import path from 'path'
 
 // import the cors
 import cors from 'cors'
@@ -10,16 +11,32 @@ dotenv.config()
 
 const app = express();
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve()
+
 await connectDb();
 // use cors middlware
-app.use(cors({
-    // it can be an array. you can whitelist different links
-    origin: 'http://localhost:5173',
-}))
+
+if(process.env.NODE_ENV !== "production"){
+
+    app.use(cors({
+        // it can be an array. you can whitelist different links
+        origin: 'http://localhost:5173',
+    }))
+}
+
 app.use(express.json());
 app.use(rateLimiter)
 app.use('/api/notes', noteRoutes)
 
+app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+
+if (process.env.NODE_ENV === "production"){
+
+    app.get("*", (req, res)=>{
+        res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'))
+    });
+}
 
 app.listen(PORT, ()=>{
     console.log(`Server is running in http://localhost:${PORT}`)
