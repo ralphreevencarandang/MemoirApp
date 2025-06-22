867a50958e17b8d39871d8595352aed988b7777b
Ralph Carandang <rvncrndng@gmail.com>
2025-06-22 19:42:52 +0800

    mod catch all route

--- backend/src/server.js
+++ backend/src/server.js
@@ -33,7 +33,7 @@ app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
 if (process.env.NODE_ENV === "production"){
 
-    app.get("*", (req, res)=>{
+    app.get("/*", (req, res)=>{
         res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'))
     });
 }
