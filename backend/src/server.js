445e30de4a846e9f2f4fa1b87f9b3723e9bd5b88
Ralph Carandang <rvncrndng@gmail.com>
2025-06-24 15:12:25 +0800

    prepare for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,27 +16,28 @@ const __dirname = path.resolve()
 await connectDb();
 // use cors middlware
 
-if(process.env.NODE_ENV !== "production"){
-
-    app.use(cors({
-        // it can be an array. you can whitelist different links
-        origin: 'http://localhost:5173',
-    }))
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
 }
 
 app.use(express.json());
 app.use(rateLimiter)
 app.use('/api/notes', noteRoutes)
 
-app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
 
-if (process.env.NODE_ENV === "production"){
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("/*", (req, res)=>{
-        res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'))
-    });
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
+
 
 app.listen(PORT, ()=>{
     console.log(`Server is running in http://localhost:${PORT}`)
