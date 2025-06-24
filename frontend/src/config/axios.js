8bd013ea87f620fe3d0a4878d5ec408b05bc7298
arar <109428773+ralphreevencarandang@users.noreply.github.com>
2025-06-24 20:45:58 +0800

    Update axios.js

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://memoirapp-xass.onrender.com/api"
 const instance = axios.create({
-    baseURL: BASE_URL
+    baseURL: "https://memoirapp-xass.onrender.com/api"
 })
 
 export default instance;
