7a8f90e96378e1b5a6b2ed84874ee04f086e4c6e
Ralph Carandang <rvncrndng@gmail.com>
2025-06-24 16:32:16 +0800

    uncomment dynamic routes

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -40,10 +40,8 @@ const HomePage = () => {
     <div className="min-h-screen">
         <Navbar/>
         {rateLimited && <RateLimitedUI/>}
-
         <div className="max-w-7xl mx-auto p-4 mt-6 ">
           {isPending && <div className="  flex justify-center items-center"> <div className="text-primary animate-spin"><Loader size={48}/></div></div>}
-
           {error && <p className="text-primary text-center font-bold">{error}</p>}
           {data?.length > 0 && !rateLimited ?  (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
